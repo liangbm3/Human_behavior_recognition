e6b9a5000ce01fd335101a80902a87b246e44082
lbm <3095088766@qq.com>
2024-11-09 13:38:02 +0800

    new

---
+++ Process_data/get_data.py
@@ -0,0 +1,69 @@
+import numpy as np
+
+# 加载四个 .npy 文件
+joint = np.load('./data/train_joint.npy')  # 形状: (N, C, T, V, M)
+bone = np.load('./data/train_bone.npy')    # 形状: (N, C, T, V, M)
+jm = np.load('./data/train_joint_motion.npy')        # 形状: (N, C, T, V, M)
+bm = np.load('./data/train_bone_motion.npy')        # 形状: (N, C, T, V, M)
+
+# 检查四个文件的形状是否一致
+assert joint.shape == bone.shape == jm.shape == bm.shape, "形状不一致！"
+
+# 拼接四个模态的 C 维度
+# 将它们沿着第 1 维（C 维）拼接，C 的总数应该是 4 * 原始的 C
+combined = np.concatenate((joint, bone, jm, bm), axis=1)  # 沿着 C 维度拼接
+
+# 检查拼接后的形状
+print("拼接后的形状：", combined.shape)  # 形状应该是 (N, 4*C, T, V, M)
+
+# 保存拼接后的结果为新的 .npy 文件
+np.save('./combined_train.npy', combined)
+
+print("拼接完成，结果保存在 'combined_data.npy")
+
+
+
+# 加载四个 .npy 文件
+joint = np.load('./data/val_joint.npy')  # 形状: (N, C, T, V, M)
+bone = np.load('./data/val_bone.npy')    # 形状: (N, C, T, V, M)
+jm = np.load('./data/val_joint_motion.npy')        # 形状: (N, C, T, V, M)
+bm = np.load('./data/val_bone_motion.npy')        # 形状: (N, C, T, V, M)
+
+# 检查四个文件的形状是否一致
+assert joint.shape == bone.shape == jm.shape == bm.shape, "形状不一致！"
+
+# 拼接四个模态的 C 维度
+# 将它们沿着第 1 维（C 维）拼接，C 的总数应该是 4 * 原始的 C
+combined = np.concatenate((joint, bone, jm, bm), axis=1)  # 沿着 C 维度拼接
+
+# 检查拼接后的形状
+print("拼接后的形状：", combined.shape)  # 形状应该是 (N, 4*C, T, V, M)
+
+# 保存拼接后的结果为新的 .npy 文件
+np.save('./combined_val.npy', combined)
+
+print("拼接完成，结果保存在 'combined_data.npy")
+
+
+
+
+# 加载四个 .npy 文件
+joint = np.load('./data/test_joint.npy')  # 形状: (N, C, T, V, M)
+bone = np.load('./data/test_bone.npy')    # 形状: (N, C, T, V, M)
+jm = np.load('./data/test_joint_motion.npy')        # 形状: (N, C, T, V, M)
+bm = np.load('./data/test_bone_motion.npy')        # 形状: (N, C, T, V, M)
+
+# 检查四个文件的形状是否一致
+assert joint.shape == bone.shape == jm.shape == bm.shape, "形状不一致！"
+
+# 拼接四个模态的 C 维度
+# 将它们沿着第 1 维（C 维）拼接，C 的总数应该是 4 * 原始的 C
+combined = np.concatenate((joint, bone, jm, bm), axis=1)  # 沿着 C 维度拼接
+
+# 检查拼接后的形状
+print("拼接后的形状：", combined.shape)  # 形状应该是 (N, 4*C, T, V, M)
+
+# 保存拼接后的结果为新的 .npy 文件
+np.save('./combined_test.npy', combined)
+
+print("拼接完成，结果保存在 'combined_data.npy")
